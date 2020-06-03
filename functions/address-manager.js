53fb08d10733ee1331aebcc45267899ca9940c1b
Meet Mehta <meet.1@iitj.ac.in>
2020-06-03 15:01:36 +0530

    Changed response and endpoints of addAddress function

--- functions/address-manager.js
+++ functions/address-manager.js
@@ -9,22 +9,30 @@ const url = process.env.DB_HOST + "/" + process.env.USER_DB;
 
 module.exports.addAddress = async function(event, context) {
     try {
-        await mongoose.connect(url);
+        const connect =  mongoose.connect(url);
         const requestBody = JSON.parse(event.body);
         const decodedUser = await jwt.verify(event.headers.authorizationToken, process.env.JWT_SECRET);
         if(decodedUser.userType !== 'customer') {
             const address = await Address.findOne({userId: decodedUser.userId});
             if(address !== null) {
-                return ResponseGenerator.generateResponse(400, "Address already present in database");
+                return ResponseGenerator.generateResponse(400, {message: "Address already present in database"});
             }
         }
         requestBody['userId'] = decodedUser.userId;
         requestBody['userType'] = decodedUser.userType;
+        requestBody['coord'] = {
+            type: "Point",
+            coordinates: requestBody['coordinates']
+        };
         const address = new Address(requestBody);
+        await connect;
         await address.save();
         return ResponseGenerator.generateResponse(200, {message: "Address added successfully"});
     } catch (err) {
-        return ResponseGenerator.generateResponse(400, {message: err.message});
+        if(err.name === "TokenExpiredError" || err.name === "JsonWebTokenError" || err.name === "NotBeforeError") {
+            return ResponseGenerator.generateResponse(400, err.message);
+        }
+        return ResponseGenerator.internalErrorResponse();
     }
 }
 
--- utils/response-generator.js
+++ utils/response-generator.js
@@ -8,3 +8,16 @@ exports.generateResponse = function (statusCode, object) {
         body: JSON.stringify(object)
     };
 };
+
+exports.internalErrorResponse = function () {
+    return {
+        statusCode: 500,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({
+            message: "Internal server error, Please try again later"
+        })
+    };
+}
